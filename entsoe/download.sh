ede0f11a6596cc565b73b9a1bc5061ac7d452d3e
martacki <martha.frysztacki@kit.edu>
2022-01-26 15:32:00 +0100

    insert new keys in download.sh

--- entsoe/download.sh
+++ entsoe/download.sh
@@ -5,7 +5,7 @@ ZOOM=${1-6}
 npm install vt-geojson
 
 export MapboxAccessToken=pk.eyJ1IjoiZW50c29lIiwiYSI6ImNpbWxxYXJocDAwMG53Ymx3N2JxNGhtZDYifQ.YjNgK9usqRNrzxWXnR152g
-for key in entsoe.1nr7sorj entsoe.5m43hs6u; do
+for key in entsoe.0ol41nja entsoe.5m43hs6u; do
     echo Getting ${key}
     node_modules/vt-geojson/cli.js ${key} -z ${ZOOM} > data/${key}-z${ZOOM}.geojson
 done
