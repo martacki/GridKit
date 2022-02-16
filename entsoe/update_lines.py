bcc7ec3d2585c2c7e3ce0ae0181bd43ef6660cbd
martacki <martha.frysztacki@kit.edu>
2022-02-16 21:40:16 +0100

    bugfix

--- entsoe/update_lines.py
+++ entsoe/update_lines.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import numpy as np
 
+def read_lines(fn):
+    return (pd.read_csv(fn, quotechar="'", true_values=['t'], false_values=['f'],
+                        dtype=dict(line_id='str', bus0='str', bus1='str',
+                                   underground="bool", under_construction="bool")))
+
 # copied from pypsa to avoid installation of pypsa in docker (too slow)
 def haversine_pts(a, b):
     """
@@ -30,31 +35,19 @@ def haversine_pts(a, b):
          np.sin((lon0 - lon1)/2.)**2)
     return 6371.000 * 2 * np.arctan2( np.sqrt(c), np.sqrt(1-c) ) * 1000
 
-buses = (pd.read_csv('result/buses.csv', quotechar="'", dtype=dict(bus_id='str')).set_index('bus_id'))
+buses = (pd.read_csv('result/buses.csv', quotechar="'",
+                     dtype=dict(bus_id='str')).set_index('bus_id'))
 
-lines = (pd.read_csv('result/lines.csv', quotechar="'", true_values=['t'], false_values=['f'],
-                     dtype=dict(line_id='str', bus0='str', bus1='str',
-                                underground="bool", under_construction="bool")))
-
-lines_3 = (pd.read_csv('database/lines_3.csv', quotechar="'", true_values=['t'], false_values=['f'],
-                       dtype=dict(line_id='str', bus0='str', bus1='str',
-                                  underground="bool", under_construction="bool")))
-
-lines_4 = (pd.read_csv('database/lines_4.csv', quotechar="'", true_values=['t'], false_values=['f'],
-                       dtype=dict(line_id='str', bus0='str', bus1='str',
-                                  underground="bool", under_construction="bool")))
-
-lines_5 = (pd.read_csv('database/lines_5.csv', quotechar="'", true_values=['t'], false_values=['f'],
-                       dtype=dict(line_id='str', bus0='str', bus1='str',
-                                  underground="bool", under_construction="bool")))
+lines = read_lines('result/lines.csv')
+lines_3 = read_lines('database/lines_3.csv')
+lines_4 = read_lines('database/lines_4.csv')
+lines_5 = read_lines('database/lines_5.csv')
 
 #adapt line lengths of corrupted lines
-lines.at[lines_3.index, "length"] = haversine_pts(buses.loc[lines.loc[lines_3.index].bus0][['x','y']],
-                                                  buses.loc[lines.loc[lines_3.index].bus1][['x','y']])
-lines.at[lines_4.index, "length"] = haversine_pts(buses.loc[lines.loc[lines_4.index].bus0][['x','y']],
-                                                  buses.loc[lines.loc[lines_4.index].bus1][['x','y']])
-lines.at[lines_5.index, "length"] = haversine_pts(buses.loc[lines.loc[lines_5.index].bus0][['x','y']],
-                                                  buses.loc[lines.loc[lines_5.index].bus1][['x','y']])
+for id in [lines_3.line_id, lines_4.line_id, lines_5.line_id]:
+    idx = lines.query("line_id in @id").index
+    lines.at[idx, "length"] = haversine_pts(buses.loc[lines.loc[idx].bus0][['x','y']],
+                                            buses.loc[lines.loc[idx].bus1][['x','y']])
 
 lines_3.length = haversine_pts(buses.loc[lines_3.bus0][['x','y']],
                                buses.loc[lines_3.bus1][['x','y']])
@@ -62,10 +55,14 @@ lines_3.length = haversine_pts(buses.loc[lines_3.bus0][['x','y']],
 lines_4.length = haversine_pts(buses.loc[lines_4.bus0][['x','y']],
                                buses.loc[lines_4.bus1][['x','y']])
 
+lines_5.length = haversine_pts(buses.loc[lines_5.bus0][['x','y']],
+                               buses.loc[lines_5.bus1][['x','y']])
+
 #adapt line indices of lines_3, _4 and _5
 lines_3.line_id = lines_3.line_id + '_2'
 lines_4.line_id = lines_4.line_id + '_3'
-lines_5.line_id = lines_4.line_id + '_4'
+lines_5.line_id = lines_5.line_id + '_4'
+
 
 #save output
 (lines
